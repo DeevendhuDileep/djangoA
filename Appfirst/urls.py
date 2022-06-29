3a2692839f41306e1562fe11fc12fb54f0d3cf55
DeevendhuDileep <deevendudileep55@gmail.com>
2022-06-29 11:06:56 +0530

    second commit

--- Appfirst/urls.py
+++ Appfirst/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('index',views.index,name='index')
 ]
